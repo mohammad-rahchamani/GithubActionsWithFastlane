92917ee3878797aa9233b962bc35bc415f5783cf
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-06-12 13:22:25 +0300

    initial commit

---
+++ GithubActionsWithFastlane/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubActionsWithFastlane
+//
+//  Created by Mohammad Rahchamani on 6/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubActionsWithFastlane/GithubActionsWithFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsWithFastlaneApp.swift
+//  GithubActionsWithFastlane
+//
+//  Created by Mohammad Rahchamani on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsWithFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
